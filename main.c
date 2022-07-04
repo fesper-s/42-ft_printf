99a4b9c90a854f09d788a2e32a40d62813815e15
fesper-s <fafaeesfabricio@gmail.com>
2022-07-04 14:04:55 -0300

    começando hexadecimal

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fesper-s <fesper-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 11:04:19 by fesper-s          #+#    #+#             */
-/*   Updated: 2022/07/01 08:10:26 by fesper-s         ###   ########.fr       */
+/*   Updated: 2022/07/04 13:11:28 by fesper-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,13 @@
 
 int	main(void)
 {
+	int	nbr;
 	int	len;
-	len = printf(" %d \n", 0);
-	printf("%d\n", len);
+	int	len2;
+
+	nbr = 422;
+	len = ft_printf("%x\n", nbr);
+	len2 = printf("%x\n", nbr);
+	printf("len = %d\nlen2 = %d\n", len, len2);
 	return (0);
 }
--- src/ft_printf_x.c
+++ src/ft_printf_x.c
@@ -6,14 +6,17 @@
 /*   By: fesper-s <fesper-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 09:47:47 by fesper-s          #+#    #+#             */
-/*   Updated: 2022/07/01 14:52:32 by fesper-s         ###   ########.fr       */
+/*   Updated: 2022/07/04 14:00:36 by fesper-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_count(int len, unsigned int nbr)
+static int	len_hex(unsigned int nbr)
 {
+	int	len;
+
+	len = 0;
 	if (!nbr)
 		len++;
 	while (nbr)
@@ -24,52 +27,17 @@ static int	ft_count(int len, unsigned int nbr)
 	return (len);
 }
 
-static char	*ft_putstr(int i, unsigned int nbr, char *str)
-{
-	str[i] = 0;
-	if (nbr == 0)
-		str[--i] = nbr + 48;
-	while (nbr > 0)
-	{
-		str[--i] = (nbr % 10) + 48;
-		nbr = nbr / 10;
-	}
-	return (&*str);
-}
-
-static char	*ft_uitoa(unsigned int n)
-{
-	int				i;
-	char			*str;
-	unsigned long	nbr;
-
-	i = 0;
-	i = ft_count(i, n);
-	nbr = (unsigned long) n;
-	if (nbr < 0)
-	{
-		nbr = nbr * -1;
-		str = malloc((++i + 1) * sizeof(char));
-		if (str == 0)
-			return (0);
-		str[0] = '-';
-	}
-	else
-	{
-		str = malloc((i + 1) * sizeof(char));
-		if (str == 0)
-			return (0);
-	}
-	str = ft_putstr(i, nbr, str);
-	return (str);
-}
-
 int	ft_printf_x(unsigned int x)
 {
-	char	*str;
+	int	len;
 
-	str = ft_uitoa(x);
-	ft_putstr_fd(str, 1);
-	free(str);
-	return (ft_strlen(str));
+	len = 0;
+	len = len_hex(x);
+	if (x >= 16)
+	{
+		ft_printf(x / 16);
+		x = x % 16;
+	}
+	ft_printf_c(x + 48);
+	return (len);
 }
